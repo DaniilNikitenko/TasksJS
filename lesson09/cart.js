2db599a8eca1660ba75119552c4d7a06c75f1c06
DaniilNikitenko <daniilnikitenko63@gmail.com>
2023-04-24 22:22:14 +0300

    lesson9

---
+++ lesson09/cart.js
@@ -0,0 +1,67 @@
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+
+  getTotalPrice() {
+    return this.totalPrice;
+  },
+  add(name, price, count = 1) {
+    this.items.push({ name, price, count: count || 1 });
+    this.totalPrice = this.calculateItemPrice();
+    this.increaseCount(1);
+  },
+  increaseCount(num) {
+    return (this.count += num);
+  },
+  calculateItemPrice() {
+    const sum = this.items.reduce((acc, item) => {
+      return acc + item.price * item.count;
+    }, 0);
+    return sum;
+  },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(`Общая стоимость корзины: ${this.totalPrice}`);
+  },
+};
+
+let user;
+
+while (user !== null) {
+  user = prompt(`Что вы хотите сделать?
+  1.Получить общую стоимость корзины
+  2.Добавить товар
+  3.Очистить корзину
+  4.Распечатать корзину`);
+
+  if (user === "1") {
+    console.log(cart.getTotalPrice());
+  }
+
+  if (user === "2") {
+    let name = prompt("Введите название товара");
+    let price = +prompt("Введите цену товара");
+    let count = +prompt("Введите количества товара товара");
+    if (Number.isNaN(price) || Number.isNaN(count) || price <= 0 || count < 0) {
+      console.log("Введите корректные значения!");
+      continue;
+    }
+
+    cart.add(name, price, count);
+  }
+
+  if (user === "3") {
+    console.log("Корзина очищена");
+    cart.clear();
+  }
+
+  if (user === "4") {
+    cart.print();
+  }
+}
